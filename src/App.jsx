8009f6bd88deaab2f1b9fd1b8ffd877ecfff4a80
huevangxp <huevang933@gmail.com>
2025-11-06 16:59:55 +0700

    commit Thu 11/06/2025 16:59:55.13

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,52 @@
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Stars, useGLTF } from "@react-three/drei";
+import { useRef } from "react";
+import { DirectionalLightHelper } from "three";
 
 function EarthModel() {
   const { scene } = useGLTF("./earth.glb");
-  return <primitive object={scene} scale={2} />;
+  const earthRef = useRef();
+
+  return (
+    <primitive
+      ref={earthRef}
+      object={scene}
+      scale={2}
+      position={[0, 0, 0]}
+      rotation={[0, 0, 0]}
+    />
+  );
 }
 
 function App() {
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
       <Canvas camera={{ position: [0, 0, 5], fov: 50 }}>
+        {/* Background */}
         <color attach="background" args={["#000"]} />
-        <ambientLight intensity={0.5} />
-        <pointLight position={[10, 10, 10]} />
+
+        {/* Soft ambient light for gentle illumination */}
+        <ambientLight intensity={0.3} />
+
+        {/* Directional light simulating sunlight */}
+        <directionalLight
+          position={[5, 3, 5]}
+          intensity={2}
+          color={"#ffffff"}
+          castShadow
+        />
+
+        {/* Secondary fill light (to brighten dark side slightly) */}
+        <pointLight position={[-5, -3, -5]} intensity={0.5} color={"#4488ff"} />
+
+        {/* Earth model */}
         <EarthModel />
-        <Stars />
-        <OrbitControls />
+
+        {/* Background stars */}
+        <Stars radius={100} depth={50} count={5000} factor={4} fade />
+
+        {/* Orbit controls to rotate camera */}
+        <OrbitControls enablePan={false} enableZoom={true} />
       </Canvas>
     </div>
   );
