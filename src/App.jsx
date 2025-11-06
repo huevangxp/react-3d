dad8848209733555fed3db537eb11748fc62c782
huevangxp <huevang933@gmail.com>
2025-11-06 16:57:30 +0700

    commit Thu 11/06/2025 16:57:30.12

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,31 @@
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls , Stars} from "@react-three/drei";
-import {useLoader} from "@react-three/fiber";
-import {TextureLoader} from "three";
+import { Canvas, useLoader } from "@react-three/fiber";
+import { OrbitControls, Stars } from "@react-three/drei";
+import { TextureLoader, DoubleSide } from "three";
 
-function App() {
-
-  const texture = useLoader(TextureLoader, "./earth.glb");
+function Earth() {
+  const texture = useLoader(TextureLoader, "./earth.jpg");
 
   return (
-    <>
+    <mesh>
+      <sphereGeometry args={[2, 64, 64]} />
+      <meshStandardMaterial map={texture} side={DoubleSide} />
+    </mesh>
+  );
+}
+
+function App() {
+  return (
     <div style={{ width: "100vw", height: "100vh" }}>
-      <Canvas  camera={{ position: [0, 0, 5] , fov: 50 }}   >
+      <Canvas camera={{ position: [0, 0, 5], fov: 50 }}>
         <color attach="background" args={["#000"]} />
         <ambientLight intensity={0.5} />
         <pointLight position={[10, 10, 10]} />
-
-        <mesh>
-        <sphereGeometry args={[2, 64, 64]} />
-        <meshStandardMaterial map={texture} side={DoubleSide} />
-        </mesh>
-
+        <Earth />
         <Stars />
         <OrbitControls />
       </Canvas>
     </div>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
