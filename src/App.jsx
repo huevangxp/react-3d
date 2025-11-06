8cfb898a1267d4c358e74645adc72e80e9b90454
huevangxp <huevang933@gmail.com>
2025-11-06 16:38:18 +0700

    commit Thu 11/06/2025 16:38:18.10

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <>
     <div style={{ width: "100vw", height: "100vh" }}>
       <Canvas style={{ width: "100vw", height: "100vh" }} camera={{ position: [0, 0, 5] }} >
-        
+        <color attach="background" args={["#f0f0f0"]} />
       </Canvas>
     </div>
     </>
