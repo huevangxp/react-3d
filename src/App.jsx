2be33499c8dc443989f95c190af635180a3ece00
huevangxp <huevang933@gmail.com>
2025-11-06 16:39:30 +0700

    commit Thu 11/06/2025 16:39:30.17

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
     <div style={{ width: "100vw", height: "100vh" }}>
-      <Canvas style={{ width: "100vw", height: "100vh" }} camera={{ position: [0, 0, 5] }} position={[0, 0, 0]} >
+      <Canvas  camera={{ position: [0, 0, 5] }}   >
         <color attach="background" args={["#f0f0f0"]} />
         <ambientLight intensity={0.5} />
         <pointLight position={[10, 10, 10]} />
