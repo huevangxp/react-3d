68b0bddaa5e490277f3202d5a51d0884e5bdf52f
huevangxp <huevang933@gmail.com>
2025-11-07 13:21:10 +0700

    commit Fri 11/07/2025 13:21:10.15

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ const App = () => {
     <div className="flex justify-center items-center h-screen">
      <div className="flex flex-col gap-2 p-4 border border-gray-300 rounded">
      <div >
+      <label htmlFor="name">Name</label>
      <input
         type="text"
         placeholder="Enter your name"
