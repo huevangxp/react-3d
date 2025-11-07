36d3db333b8248a37322ce8b448c40fcaff33d45
huevangxp <huevang933@gmail.com>
2025-11-07 16:53:58 +0700

    commit Fri 11/07/2025 16:53:58.16

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { AgFinancialCharts } from "ag-charts-react";
 import "ag-charts-enterprise";
-import axios from "axios";
+ import getData from "./data";
 
 function App() {
-  
-
-  const randomData = [
-    {
-      date: new Date(2025, 2, 1),
-      open: 140.039993,
-      high: 140.479996,
-      low: 133.580002,
-      close: 136.449997,
-      volume: 36840400,
-    },
-  ];
-
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        const response = await axios.get("https://www.goldapi.io/api/XAU/USD/20230617");
-        console.log(response.data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-    getData();
-  }, []);
-
+ 
   const [options, setOptions] = useState({
-    data: randomData,
+    data: getData,
   });
 
   return (
