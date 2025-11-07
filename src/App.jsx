fe1d1cb5b204639146932efce8707e41cffedd2a
huevangxp <huevang933@gmail.com>
2025-11-07 14:24:03 +0700

    commit Fri 11/07/2025 14:24:03.19

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,20 @@ import { AgFinancialCharts } from "ag-charts-react";
 import "ag-charts-enterprise";
 
 function App() {
+
+  const randomData = [
+    {
+      date: new Date(2022, 2, 1),
+      open: 140.039993,
+      high: 140.479996,
+      low: 133.580002,
+      close: 136.449997,
+      volume: 36840400,
+    },
+  ];
+
   const [options, setOptions] = useState({
-    data: [
-      {
-        date: new Date(2022, 2, 1),
-        open: 140.039993,
-        high: 140.479996,
-        low: 133.580002,
-        close: 136.449997,
-        volume: 36840400,
-      },
-    ],
+    data: randomData,
   });
 
   return (
