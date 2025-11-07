b3efbd00bbec4459530e8327919304faa62c4b8a
huevangxp <huevang933@gmail.com>
2025-11-07 13:20:24 +0700

    commit Fri 11/07/2025 13:20:24.12

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
   const [name, setName] = useState("");
   return (
     <div className="flex justify-center items-center h-screen">
-     <div className="flex gap-2">
+     <div className="flex flex-col gap-2">
      <div >
      <input
         type="text"
