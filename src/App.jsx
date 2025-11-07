eada27ba5fbffbfb7fdb8f8808717c0ab13871aa
huevangxp <huevang933@gmail.com>
2025-11-07 11:10:29 +0700

    commit Fri 11/07/2025 11:10:29.14

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Car from './components/Car'
 const App = () => {
   return (
     <div>
-       <div className="flex justify-center items-center">
+       <div className="flex justify-center items-center w-screen h-screen">
         <div>
           <h1>Car</h1>
         </div>
