d930ef71684446db9daf5f2b4586d8f7d9864a4f
huevangxp <huevang933@gmail.com>
2025-11-07 13:22:27 +0700

    commit Fri 11/07/2025 13:22:27.17

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,23 @@ const App = () => {
   const [name, setName] = useState("");
   return (
     <div className="flex justify-center items-center h-screen">
-     <div className="flex flex-col gap-2 p-4 border border-gray-300 rounded">
-     <div className="flex flex-row gap-2" >
-      <div>
-      <label htmlFor="name">Name</label>
-      </div>
- <div>
- <input
-        type="text"
-        placeholder="Enter your name"
-        onChange={(e) => setName(e.target.value)}
-        value={name}
-        className="border border-gray-300 rounded p-2"
-      />
- </div>
-     </div>
-      <button onClick={() => console.log(name)} className="bg-blue-500 text-white p-2 rounded">Submit</button>
-     </div>
+   <div className="flex flex-col gap-2 p-4 border border-gray-300 rounded">
+  <label htmlFor="name">Name</label>
+  <input
+    id="name"
+    type="text"
+    placeholder="Enter your name"
+    onChange={(e) => setName(e.target.value)}
+    value={name}
+    className="border border-gray-300 rounded p-2"
+  />
+  <button
+    onClick={() => console.log(name)}
+    className="bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
+  >
+    Submit
+  </button>
+</div>
     </div>
   );
 };
