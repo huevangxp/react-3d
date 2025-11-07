d1f6b1f9778101f5968807fcb726cfad72c435f6
huevangxp <huevang933@gmail.com>
2025-11-07 11:15:04 +0700

    commit Fri 11/07/2025 11:15:04.16

--- src/components/Car.jsx
+++ src/components/Car.jsx
@@ -17,7 +17,7 @@ function CarModel() {
       ref={carRef}
       object={scene}
       scale={2}
-      position={[0, -1, 0]}
+      position={[0, 0, 0]}
       rotation={[0, 0, 0]}
       onClick={handleCarClick}
     />
