44fc5df7f066cded82b2f82994803406e3ea475b
huevangxp <huevang933@gmail.com>
2025-11-07 10:33:55 +0700

    commit Fri 11/07/2025 10:33:55.10

--- src/components/Sun.jsx
+++ src/components/Sun.jsx
@@ -1,21 +1,15 @@
 // Sun.jsx
 import { useRef } from "react";
-import { MeshStandardMaterial } from "three";
+import { useGLTF } from "@react-three/drei";
+
+
 
 function Sun() {
   const sunRef = useRef();
-
+  const { scene } = useGLTF("./sun.glb");
   return (
     <mesh ref={sunRef} position={[8, 3, 0]}>
-      {/* Create a glowing sphere for the sun */}
-      <sphereGeometry args={[1.5, 64, 64]} />
-      <meshStandardMaterial
-        emissive={"#ffff66"} // glowing yellow light
-        emissiveIntensity={2}
-        color={"#ffcc33"}
-      />
-      {/* Optional light emitted from sun */}
-      <pointLight intensity={2.5} distance={50} color={"#ffd700"} />
+     
     </mesh>
   );
 }
