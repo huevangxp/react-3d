812052ff7fcd968384cd617f70349c3a8a9b3561
huevangxp <huevang933@gmail.com>
2025-11-07 13:19:45 +0700

    commit Fri 11/07/2025 13:19:45.19

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ const App = () => {
   return (
     <div className="flex justify-center items-center h-screen">
      <div className="flex gap-2">
+     <div>
      <input
         type="text"
         placeholder="Enter your name"
         onChange={(e) => setName(e.target.value)}
       />
+     </div>
       <button>Submit</button>
      </div>
     </div>
