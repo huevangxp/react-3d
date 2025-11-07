9918d128e71c0d76677eecf7f474219439296999
huevangxp <huevang933@gmail.com>
2025-11-07 16:17:48 +0700

    commit Fri 11/07/2025 16:17:48.19

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { AgFinancialCharts } from "ag-charts-react";
 import "ag-charts-enterprise";
+import axios from "axios";
 
 function App() {
   
@@ -16,7 +17,17 @@ function App() {
     },
   ];
 
- 
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axios.get("http://lsx.com.la/lo/market/daily-closing-price");
+        console.log(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    getData();
+  }, []);
 
   const [options, setOptions] = useState({
     data: randomData,
