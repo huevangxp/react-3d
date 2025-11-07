bc1996451fb59653257e5d28b802efbb3acf248b
huevangxp <huevang933@gmail.com>
2025-11-07 14:15:12 +0700

    commit Fri 11/07/2025 14:15:12.18

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { AgFinancialCharts } from "ag-charts-react";
 import "ag-charts-enterprise";
-import getData from "./data";
+import getData from "../public/data";
 
 function App() {
   const [options, setOptions] = useState({
