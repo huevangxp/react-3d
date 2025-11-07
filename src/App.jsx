187e8e9a523b7a844dfff2f546140de22ec3fec1
huevangxp <huevang933@gmail.com>
2025-11-07 14:13:42 +0700

    commit Fri 11/07/2025 14:13:42.16

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,13 @@
 import React, { useState } from "react";
-import { createRoot } from "react-dom/client";
-import { AgCharts } from "ag-charts-react";
+import { AgFinancialCharts } from "ag-charts-react";
 import "ag-charts-enterprise";
 
-const ChartExample = () => {
+function App() {
   const [options, setOptions] = useState({
     data: getData(),
-    title: {
-      text: "S&P 500 Index",
-    },
-    subtitle: {
-      text: "Daily High and Low Prices",
-    },
-    footnote: {
-      text: "1 Aug 2023 - 1 Nov 2023",
-    },
-    series: [
-      {
-        type: "candlestick",
-        xKey: "date",
-        xName: "Date",
-        lowKey: "low",
-        highKey: "high",
-        openKey: "open",
-        closeKey: "close",
-      },
-    ],
   });
 
-  return <AgCharts options={options} />;
+  return <AgFinancialCharts options={options} />;
 };
 
-const root = createRoot(document.getElementById("root"));
-root.render(<ChartExample />);
+export default App;
