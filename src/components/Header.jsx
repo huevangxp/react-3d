52c38948806412c2ffac5941d5f68420b65ae771
huevangxp <huevang933@gmail.com>
2025-11-08 09:19:56 +0700

    commit Sat 11/08/2025  9:19:56.12

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,17 +3,19 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-        <nav className="navbar d-flex justify-content-between">
+        <nav className="navbar flex justify-content-between">
             <div className="logo">
                <h1>AG Charts</h1>
             </div>
-            <ul>
+           <div>
+           <ul>
                 <li>Home</li>
                 <li>Products</li>
                 <li>Services</li>
                 <li>About</li>
                 <li>Contact</li>
             </ul>
+           </div>
         </nav>
     </div>
   )
