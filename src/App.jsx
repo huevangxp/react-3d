b9bf2f360d4a602125389c7014a7462a0e1be782
huevangxp <huevang933@gmail.com>
2025-11-08 09:07:37 +0700

    commit Sat 11/08/2025  9:07:37.20

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,14 @@ import getData from "../public/data";
 
 function App() {
 
-  const [darkMode, setDarkMode] = useState(false);
-
+  
   const [options, setOptions] = useState({
     data: getData(),
-    theme: darkMode ? "ag-theme-dark" : "ag-theme-default",
   });
 
   return (
-    <div style={{ width: "100vw", height: "100vh", backgroundColor: darkMode ? "#000" : "#fff" }}>
-      <button onClick={() => setDarkMode(!darkMode)}>Toggle Dark Mode</button>
-      <AgFinancialCharts options={options} onOptionsChange={setOptions} />
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <AgFinancialCharts options={options} />
     </div>
   );
 }
