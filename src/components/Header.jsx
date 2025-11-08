a38652a41630fc38f158a5a2ecfc6ee006844698
huevangxp <huevang933@gmail.com>
2025-11-08 09:21:14 +0700

    commit Sat 11/08/2025  9:21:14.21

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-        <nav className="navbar flex justify-between p-6 bg-black text-white mb-2">
+        <nav className="navbar flex justify-between p-6 bg-black text-white mb-2 shadow-lg">
             <div className="logo">
                <h1 className='text-2xl font-bold'>AG Charts</h1>
             </div>
