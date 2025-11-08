4d6b3639610f0989f3ab0376bf1a520c57c24156
huevangxp <huevang933@gmail.com>
2025-11-08 09:06:37 +0700

    commit Sat 11/08/2025  9:06:37.12

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ import "ag-charts-enterprise";
 import getData from "../public/data";
 
 function App() {
+
+  const [darkMode, setDarkMode] = useState(false);
+
   const [options, setOptions] = useState({
     data: getData(),
   });
 
   return (
-    <div style={{ width: "100vw", height: "100vh" }}>
+    <div style={{ width: "100vw", height: "100vh", backgroundColor: darkMode ? "#000" : "#fff" }}>
       <AgFinancialCharts options={options} />
     </div>
   );
