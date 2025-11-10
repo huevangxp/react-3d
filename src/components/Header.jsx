60e538e2425ccc630c6fcebcb50886a481e638e1
huevangxp <huevang933@gmail.com>
2025-11-10 08:26:29 +0700

    commit Mon 11/10/2025  8:26:29.12

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-// import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const Header = () => {
-//   const navigate = useNavigate()
+  const navigate = useNavigate()
 
   const navItems = [
     { name: 'Home', path: '/pages/home' },
